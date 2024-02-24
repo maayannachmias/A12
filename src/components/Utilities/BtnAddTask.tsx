d1b3c65a93e641e5826307df229e59c8b8542e3b
maayannachmias <maayansharvit1997@gmail.com>
2024-02-24 12:46:29 +0200

    HW2

---
+++ src/components/Utilities/BtnAddTask.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useAppDispatch } from "../../store/hooks";
+import { modalActions } from "../../store/Modal.store";
+
+const BtnAddTask: React.FC<{ className?: string }> = ({ className }) => {
+  const dispatch = useAppDispatch();
+
+  const onOpenModal = () => {
+    dispatch(modalActions.openModalCreateTask());
+  };
+
+  return (
+    <button
+      className={`btn ${className}`}
+      onClick={onOpenModal}
+    >
+      {/* For small to medium screens, show only "+" */}
+      <span className="xl:hidden ">+</span>
+      {/* For large screens and up, show "Add New Task" */}
+      <span className="hidden xl:inline">Add New Task</span>
+    </button>
+  );
+};
+
+export default BtnAddTask;
